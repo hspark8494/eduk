71cd04afe193e8e0c3c8245039c95e36aba448e9
Kimsungkyu95 <76465997+Kimsungkyu95@users.noreply.github.com>
2021-12-07 11:06:40 +0900

    file update

--- src/main/java/com/eduk/controller/FilesController.java
+++ src/main/java/com/eduk/controller/FilesController.java
@@ -11,38 +11,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eduk.domain.Files;
+import com.eduk.dto.FileDTO;
 
 @RestController
 @RequestMapping("/file")
 public class FilesController {
 	
+	private final String SAVE_PATH="/Users/sungkyu-kim/springDownload";
+	
 	/**
 	 * 파일 업로드
 	 */
 	@PostMapping("/uploadfile")
-	public void uploadFile(@RequestParam("file") MultipartFile file) { 
-		if(file.isEmpty()){ //파일 업로드가 안됐을 시 처리 
-			
-//		}
-//			String fileName = fileStorageService.storeFile(file); 
-//			//확장자만 추출하는 형태 ex) exe , png, jpg ... 
-//			String fileExt = fileName.replaceAll("^.*\\.(.*)$", "$1"); 
-//			String fileOriginalName = StringUtils.cleanPath(file.getOriginalFilename()); 
-//			String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//					.path("/file/downloadfile/")
-//					.path(fileName)
-//					.toUriString(); 
-//			return new File(fileName, fileOriginalName, fileExt, fileDownloadUri); 
-			}
+	public FileDTO uploadFile(FileDTO fileDTO, HttpSession session) { 
+		//저장할 폴더 설정
+	    //String path = session.getServletContext().getRealPath("/WEB-INF/save");
+	    
+	    MultipartFile file = fileDTO.getFile();
+	    if(file.getSize()>0)
+		try {
+	      file.transferTo(new File(SAVE_PATH+"/" + file.getOriginalFilename()));
+	      
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		fileDTO.setFileName(file.getOriginalFilename());
+		fileDTO.setOriginalFileName(file.getOriginalFilename());
+		fileDTO.setFileSize(file.getSize());
+		
+		return fileDTO;
 	}
 	
 	/**
 	 * 파일 다운로드
 	 */
 	@GetMapping("/download")
-	public void download() {
-		
+	public ModelAndView download(String fname) {
+		File file = new File(SAVE_PATH+"/"+fname);
+		return new ModelAndView("downLoadView","fname", file);
 	}
 }
---
+++ src/main/java/com/eduk/dto/FileDTO.java
@@ -0,0 +1,24 @@
+package com.eduk.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileDTO {
+	  private String name;
+	  private MultipartFile file; //폼에서 name="file"
+	  
+	  private String fileName;
+	  private String originalFileName;
+	  
+	  private long fileSize;
+}
+
+
