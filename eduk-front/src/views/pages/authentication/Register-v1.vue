d53c8c83ce505632f4b2ba10a925d309427eba5c
hspark8494 <hspark8494@gmail.com>
2021-12-10 11:26:12 +0900

    update

--- eduk-front/src/views/pages/authentication/Register-v1.vue
+++ eduk-front/src/views/pages/authentication/Register-v1.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="auth-wrapper auth-v1 px-2">
     <div class="auth-inner py-2">
+
       <!-- Register v1 -->
       <b-card class="mb-0">
         <b-link class="brand-logo">
           <vuexy-logo />
-<<<<<<< HEAD
-          <h2 class="brand-text text-primary ml-1">EduK</h2>
-        </b-link>
-
-        <b-card-title class="mb-1"> Eduk를 즉시 이용해보세요~! </b-card-title>
-=======
           <h2 class="brand-text text-primary ml-1">
             eduK
           </h2>
@@ -19,23 +14,21 @@
         <b-card-title class="mb-1">
           eduk를 즉시 이용해보세요~!
         </b-card-title>
->>>>>>> 1d2eea238245b219632ae3314207ead7c9058f2a
         <b-card-text class="mb-2">
           eduk는 편리하게 회의를 열어 참여하거나 교육을 수강할 수 있습니다.
         </b-card-text>
 
         <!-- form -->
         <validation-observer ref="registerForm">
-          <b-form class="auth-register-form mt-2" @submit.prevent="register">
+          <b-form
+            class="auth-register-form mt-2"
+            @submit.prevent="register"
+          >
             <!-- username -->
-<<<<<<< HEAD
-            <b-form-group label="Username" label-for="username">
-=======
             <b-form-group
               label="이름"
               label-for="username"
             >
->>>>>>> 1d2eea238245b219632ae3314207ead7c9058f2a
               <validation-provider
                 #default="{ errors }"
                 name="Username"
@@ -44,7 +37,7 @@
                 <b-form-input
                   id="username"
                   v-model="username"
-                  :state="errors.length > 0 ? false : null"
+                  :state="errors.length > 0 ? false:null"
                   name="register-username"
                   placeholder="johndoe"
                 />
@@ -52,14 +45,10 @@
             </b-form-group>
 
             <!-- email -->
-<<<<<<< HEAD
-            <b-form-group label="Email" label-for="email">
-=======
             <b-form-group
               label="이메일"
               label-for="email"
             >
->>>>>>> 1d2eea238245b219632ae3314207ead7c9058f2a
               <validation-provider
                 #default="{ errors }"
                 name="Email"
@@ -68,7 +57,7 @@
                 <b-form-input
                   id="email"
                   v-model="regEmail"
-                  :state="errors.length > 0 ? false : null"
+                  :state="errors.length > 0 ? false:null"
                   name="register-email"
                   placeholder="john@example.com"
                 />
@@ -76,14 +65,10 @@
             </b-form-group>
 
             <!-- password -->
-<<<<<<< HEAD
-            <b-form-group label="Password" label-for="password">
-=======
             <b-form-group
               label="비밀번호"
               label-for="password"
             >
->>>>>>> 1d2eea238245b219632ae3314207ead7c9058f2a
               <validation-provider
                 #default="{ errors }"
                 name="Password"
@@ -91,13 +76,13 @@
               >
                 <b-input-group
                   class="input-group-merge"
-                  :class="errors.length > 0 ? 'is-invalid' : null"
+                  :class="errors.length > 0 ? 'is-invalid':null"
                 >
                   <b-form-input
                     id="password"
                     v-model="password"
                     :type="passwordFieldType"
-                    :state="errors.length > 0 ? false : null"
+                    :state="errors.length > 0 ? false:null"
                     class="form-control-merge"
                     name="register-password"
                     placeholder="············"
@@ -115,24 +100,17 @@
 
             <!-- checkbox -->
             <b-form-group>
-              <b-link>개인정보보호정책 및 약관</b-link>
               <b-form-checkbox
                 id="register-privacy-policy"
                 v-model="status"
                 name="checkbox-1"
               >
-<<<<<<< HEAD
-=======
                 <b-link>개인정보보호정책 및 약관</b-link>
->>>>>>> 1d2eea238245b219632ae3314207ead7c9058f2a
                 에 동의합니다.
               </b-form-checkbox>
             </b-form-group>
 
             <!-- submit button -->
-<<<<<<< HEAD
-            <b-button variant="primary" block type="submit"> 제출 </b-button>
-=======
             <b-button
               variant="primary"
               block
@@ -140,50 +118,35 @@
             >
               완료
             </b-button>
->>>>>>> 1d2eea238245b219632ae3314207ead7c9058f2a
           </b-form>
         </validation-observer>
 
         <b-card-text class="text-center mt-2">
           <span>이미 계정을 가지고 계신가요? </span>
-          <b-link :to="{ name: 'auth-login-v1' }">
-            <span>로그인하세요!</span>
+          <b-link :to="{name:'auth-login-v1'}">
+            <span>로그인하세요~!</span>
           </b-link>
         </b-card-text>
 
-<<<<<<< HEAD
-        <div class="divider my-2">
-          <div class="divider-text">or</div>
-        </div>
-=======
->>>>>>> 1d2eea238245b219632ae3314207ead7c9058f2a
       </b-card>
-      <!-- /Register v1 -->
+    <!-- /Register v1 -->
     </div>
   </div>
+
 </template>
 
 <script>
-import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
 import {
-  BCard,
-  BLink,
-  BCardTitle,
-  BCardText,
-  BForm,
-  BButton,
-  BFormInput,
-  BFormGroup,
-  BInputGroup,
-  BInputGroupAppend,
-  BFormCheckbox,
-} from "bootstrap-vue";
-import VuexyLogo from "@core/layouts/components/Logo.vue";
-import { required, email } from "@validations";
-import { togglePasswordVisibility } from "@core/mixins/ui/forms";
-import ToastificationContent from "@core/components/toastification/ToastificationContent.vue";
+  BCard, BLink, BCardTitle, BCardText, BForm,
+  BButton, BFormInput, BFormGroup, BInputGroup, BInputGroupAppend, BFormCheckbox,
+} from 'bootstrap-vue'
+import VuexyLogo from '@core/layouts/components/Logo.vue'
+import { required, email } from '@validations'
+import { togglePasswordVisibility } from '@core/mixins/ui/forms'
+import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
 
-import useJwt from "@/auth/jwt/useJwt";
+import useJwt from '@/auth/jwt/useJwt'
 
 export default {
   components: {
@@ -207,60 +170,59 @@ export default {
   mixins: [togglePasswordVisibility],
   data() {
     return {
-      regEmail: "",
-      username: "",
-      password: "",
-      status: "",
+      regEmail: '',
+      username: '',
+      password: '',
+      status: '',
 
       // validation rules
       required,
       email,
-    };
+    }
   },
   computed: {
     passwordToggleIcon() {
-      return this.passwordFieldType === "password" ? "EyeIcon" : "EyeOffIcon";
+      return this.passwordFieldType === 'password' ? 'EyeIcon' : 'EyeOffIcon'
     },
   },
   methods: {
     register() {
-      this.$refs.registerForm.validate().then((success) => {
+      this.$refs.registerForm.validate().then(success => {
         if (success) {
-          useJwt
-            .register({
-              name: this.username,
-              email: this.regEmail,
-              password: this.password,
-            })
-
-            .then((response) => {
+          useJwt.register({
+            name : this.username,
+            email: this.regEmail,
+            password: this.password,
+          })
+          
+            .then(response => {
               console.log(response);
-              useJwt.setToken(response.data.accessToken);
-              useJwt.setRefreshToken(response.data.refreshToken);
+              useJwt.setToken(response.data.accessToken)
+              useJwt.setRefreshToken(response.data.refreshToken)
               // localStorage.setItem('userData', JSON.stringify(response.data.userData))
               // this.$ability.update(response.data.userData.ability)
-              this.$router.push("/");
+              this.$router.push('/')
 
-              //TODO 기현 : 모달창이나 이메일 인증창으로 넘겨주기
-              this.$toast({
-                component: ToastificationContent,
-                props: {
-                  title: "Form Submitted",
-                  icon: "EditIcon",
-                  variant: "success",
-                },
-              });
+              //TODO 기현 : 모달창이나 이메일 인증창으로 넘겨주기 
+                        this.$toast({
+            component: ToastificationContent,
+            props: {
+              title: 'Form Submitted',
+              icon: 'EditIcon',
+              variant: 'success',
+            },
+          })
             })
-            .catch((error) => {
-              this.$refs.registerForm.setErrors(error.response.data.error);
-            });
+            .catch(error => {
+              this.$refs.registerForm.setErrors(error.response.data.error)
+            })
         }
-      });
+      })
     },
   },
-};
+}
 </script>
 
 <style lang="scss">
-@import "@core/scss/vue/pages/page-auth.scss";
+@import '@core/scss/vue/pages/page-auth.scss';
 </style>
